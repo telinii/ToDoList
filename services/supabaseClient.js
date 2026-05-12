0c91a665ca850f902e1aa8ac815fcf679954760f
Luca Telini Crozara <gply714@gmail.com>
2026-05-12 16:19:05 -0300

    Definitive

--- services/supabaseClient.js
+++ services/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const supabaseUrl = "https://edwtytgeoemtjnnzopev.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkd3R5dGdlb2VtdGpubnpvcGV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg1MDc0MTMsImV4cCI6MjA5NDA4MzQxM30.Ij4VkrYk44OYVDRTOaQKiFVCsRnSInlJOwzCmq2hakU";
+const supabaseUrl = "URL-DO-SEU-DATABASE-NO-SUPABASE";
+const supabaseKey = "SUA-ANON-KEY";
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
